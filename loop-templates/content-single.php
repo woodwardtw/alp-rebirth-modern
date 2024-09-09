070c2bc82f6d7973c15bf896c7791527f2c7e66c
woodwardtw <twwoodward@me.com>
2024-09-09 14:37:11 -0400

    fix article

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -1,35 +1,62 @@
 <?php
 /**
- * Single post partial template
+ * Single post partial template.
  *
- * @package Understrap
+ * @package understrap
  */
 
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
 ?>
+</div>
+</div>
+</div>
+<div class="jumbotron jumbotron-story">
+	 <div class="container story header">
+	 	<div class="justify-content-md-center row">	 		
+	 		<div class="col-md-9 the-story-header-text">
+	 			<div class="the-story-tag">Our Stories</div>
+          		<h1 class="display-3 the-story-title"><?php the_title();?></h1>
+          		<div class="author-block">
+          			<img class="author-img" src="<?php echo get_avatar_url(get_the_author_meta('ID'));?>">
+          			<div class="the-story-author"><?php echo get_the_author_meta('display_name');?></div>
+          		</div>
+	 		</div>	 	  
+        </div>
+    </div>
+</div>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<div class="container the-story">
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">	
+	<?php echo get_the_story_lead();?>
 
-	<header class="entry-header">
+	<?php echo get_the_post_thumbnail( $post->ID, 'story', array( 'class' => 'img-fluid' ) ); ?>
+	<div class="the-story-caption"><?php echo get_the_post_thumbnail_caption();?></div>
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-
-			<?php understrap_posted_on(); ?>
-
-		</div><!-- .entry-meta -->
-
-	</header><!-- .entry-header -->
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
-	<div class="entry-content">
+	<div class="entry-content row align-items-center">
+		<div class="row">
+			<div class="col-md-8 offset-md-2">
+				<?php the_content(); ?>
+			</div>
+			<div class="col-md-4 offset-md-2 story-meta">
+				<h3>Story tags</h3>				
+				<?php single_story_tags($post->ID);?>
+				<?php single_story_real_tags($post->ID);?>
+			</div>
+			<div class="col-md-4 story-meta">
+				<h3>Get in touch</h3>
+				<?php echo story_social_links(get_the_author_meta('display_name'));?>
+			</div>
+			<div class="col-md-8 offset-md-2 story-meta soc-share-box">
+				<h3>Share this story</h3>
+				<?php echo story_social_sharing_buttons($post)?>
+			</div>
+		</div>
+		
 
 		<?php
-		the_content();
-		understrap_link_pages();
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+			'after'  => '</div>',
+		) );
 		?>
 
 	</div><!-- .entry-content -->
@@ -40,4 +67,4 @@ defined( 'ABSPATH' ) || exit;
 
 	</footer><!-- .entry-footer -->
 
-</article><!-- #post-<?php the_ID(); ?> -->
+</article><!-- #post-## -->
